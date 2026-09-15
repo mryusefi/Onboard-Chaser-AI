59f16ae55d8366c3925495ef7668f4c7451c6e12
mryusefi <mohammadreza.yousefi.2003@gmail.com>
2026-09-16 02:11:06 +0330

    feat: theme context + toggle component, dark tokens, pre-paint init script (Part D infra)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,17 +6,20 @@ import App from './App.jsx'
 import { ToastProvider } from './context/ToastContext'
 import { OnboardingProvider } from './context/OnboardingContext'
 import { AuthProvider } from './context/AuthContext'
+import { ThemeProvider } from './context/ThemeContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <AuthProvider>
-        <ToastProvider>
-          <OnboardingProvider>
-            <App />
-          </OnboardingProvider>
-        </ToastProvider>
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <ToastProvider>
+            <OnboardingProvider>
+              <App />
+            </OnboardingProvider>
+          </ToastProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 )
